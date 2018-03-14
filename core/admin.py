96c17f92bcf86dd5b324dcc90580fbd2ad8eedbe
vitawood <vekeh@hotmail.co.uk>
2018-03-14 18:05:25 +0000

    conflicts prob

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Category, Tea, Review
+ 
+ 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-# Register your models here.
+admin.site.register(Category, CategoryAdmin)
