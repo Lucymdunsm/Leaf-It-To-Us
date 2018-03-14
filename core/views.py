87b51f68ffcfa115214cf234443868f368128a18
CameronNicolson <cameronnicolson50@gmail.com>
2018-03-14 17:40:33 +0000

    New Views class methods, client-side script main.js, and html templates
    ✔️ Views.py - show_account, search, show_catalog return associated html
    templates. most_popular method returns a JSON response
    
    ✔️ Main.js - Main JavaScript code for site. Attaches event handler -
    restAPI method creates AJAX requests to the server, further to be
    supported for different endpoints. refresh_listView refreshes elements on
    pages in response to updates from the server when filtering content.
    
    ✔️ catalog.html, search.html, account.html are new files all the other
    modifed html files now include template data bindings to support Views
    
    ✔️ Settings.py - new processor to support media
    
    ✔️ Urls.py - uncommented url patterns lines - leaf_it_to_us/settings.py url
    pattern for media

--- core/views.py
+++ core/views.py
@@ -1,14 +1,68 @@
+import json
+from django.core import serializers
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from core.models import Tea, User, UserProfile
 
 def home(request):
     return render(request, 'tea/base.html') #have this direct to home once that's been made
 
-def search(request):
-    return render(request, 'tea/search.html')
-
 def faq(request):
     return render(request, 'tea/faq.html')
 
-def teas(request):
-    return render(request, 'tea/teas.html')
+def show_catalog(request):
+	context_dict = {}
+	try:
+		teaList = Tea.objects.order_by('name')
+		context_dict["teas"] = teaList
+	except Tea.DoesNotExist:
+		teaList["teas"] = None
+
+	response = render(request, 'tea/catalog.html', context_dict)
+	return response
+
+def most_popular(request):
+	context_dict = {}
+	if request.method == 'GET':
+		try:
+			teaList = Tea.objects.order_by('-views')
+			context_dict["teas"] = teaList
+		except Tea.DoesNotExist:
+			teaList["teas"] = None
+			context_dict["teas"] = teaList
+
+	Tea_json = serializers.serialize("json", teaList)
+	data = {"data": Tea_json}
+	return JsonResponse(data)
+
+def search(request):
+	matches = {}
+	if request.method == 'POST':
+	# make client's request lowercase
+		query = request.POST.get('query').lower()
+		print(query)
+		try:
+		# interface with db
+			results = Tea.objects.filter(name__icontains=query)
+			matches["results"] = results 
+		except Tea.DoesNotExist:
+		# Execption, return nothing
+			matches["results"] = None
+
+	print(matches["results"])
+	return render(request, 'tea/search.html', matches)
+
+def show_account(request):
+	account = {}
+	# TO BE REMOVED 
+	# This line is for development purposes only
+	# MeghanBright account 
+	meghan = "MeghanBright"
+	try:
+		user = User.objects.get(username=meghan)
+		profile = UserProfile.objects.get(user=user)
+		account = {"user": user, "profile": profile}
+	except User.DoesNotExist:
+		account = {"user": None, "profile": None}
+
+	return render(request, 'tea/account.html', account)
--- leaf_it_to_us/urls.py
+++ leaf_it_to_us/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import include
 from core import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^$', views.home, name = 'home'),
     re_path(r'^leafittous/', include('core.urls')),
     re_path(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
